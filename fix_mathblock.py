4c43120790a62fde7a930a8829e45f5e58650cd6
ashawkey <ashawkey1999@gmail.com>
2025-05-18 10:07:42 +0800

    update

--- fix_mathblock.py
+++ fix_mathblock.py
@@ -9,48 +9,52 @@ files = glob.glob('docs/**/*.md', recursive=True)
 # 2. always wrap all content within \displaylines{}
 
 for file in files:
-    with open(file, 'r') as f:
-        lines = f.readlines()
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
 
-    # naive matching $$...$$
-    outside = True
-    modified = False
-    for i, line in enumerate(lines):
-        if re.match('^\$\$', line) is not None:
-            if outside:
-                if lines[i+1] != '\displaylines{\n':
-                    lines[i] = '\n$$\n\displaylines{\n'
-                    modified = True
-                outside = False
-            else:
-                if lines[i-1] != '}\n':
-                    lines[i] = '}\n$$\n\n'
-                    modified = True
-                outside = True
+        # naive matching $$...$$
+        outside = True
+        modified = False
+        for i, line in enumerate(lines):
+            if re.match(r'^\$\$', line) is not None:
+                if outside:
+                    if lines[i+1] != '\\displaylines{\n':
+                        lines[i] = '\n$$\n\\displaylines{\n'
+                        modified = True
+                    outside = False
+                else:
+                    if lines[i-1] != '}\n':
+                        lines[i] = '}\n$$\n\n'
+                        modified = True
+                    outside = True
 
-        # remove blank lines inside
-        if not outside and re.match('^\s*\n$', line):
-            lines[i] = ''
-            modified = True
-            
-    # remove excessive blank lines
-    cnt = 0
-    for i, line in enumerate(lines):
-        if re.match('^\s*\n$', line):
-            cnt += 1
-            if cnt >= 3:
+            # remove blank lines inside
+            if not outside and re.match(r'^\s*\n\$', line):
                 lines[i] = ''
                 modified = True
-        else:
-            cnt = 0
+                
+        # remove excessive blank lines
+        cnt = 0
+        for i, line in enumerate(lines):
+            if re.match(r'^\s*\n\$', line):
+                cnt += 1
+                if cnt >= 3:
+                    lines[i] = ''
+                    modified = True
+            else:
+                cnt = 0
 
-    if modified:
-        lines = [line for line in lines if line != '']
-    
-    if not outside:
-        print(f'[WARN] {file} unmatched $$, no modification is done.')
-    else:
         if modified:
-            with open(file, 'w') as f:
-                f.writelines(lines)
-            print(f'[INFO] {file} fixed.')
+            lines = [line for line in lines if line != '']
+        
+        if not outside:
+            print(f'[WARN] {file} unmatched $$, no modification is done.')
+        else:
+            if modified:
+                with open(file, 'w', encoding='utf-8') as f:
+                    f.writelines(lines)
+                print(f'[INFO] {file} fixed.')
+    except Exception as e:
+        print(f'[ERROR] {file}: {e}')
+        break
